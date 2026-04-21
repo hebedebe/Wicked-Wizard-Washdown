2ab8c589da1fc623209cc2a5160ade0cebf08ef7
Oliver Alcaraz <s242289@students.aie.edu.au>
2026-04-21 14:49:56 +1000

    start animation

--- Source/WickedWizardWashdown/Private/Wizard/Structs/WizardCustomisationData.h
+++ Source/WickedWizardWashdown/Private/Wizard/Structs/WizardCustomisationData.h
@@ -10,14 +10,14 @@ struct FWizardCustomisationData
 	
 public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Colours")
-	FColor HatColour = FColor::Purple;
+	FColor HatColour = FColor::Yellow;
 	
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Colours")
-	FColor HeadColour = FColor::Black;
+	FColor HeadColour = FColor::Green;
 	
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Colours")
 	FColor BodyColour = FColor::Purple;
 	
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Colours")
-	FColor HandColour = FColor::White;
+	FColor HandColour = FColor::Red;
 };
